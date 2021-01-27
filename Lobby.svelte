2286f5dd7ffa94bac3ea871cd78e325337846577
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2021-01-26 20:50:24 -0500

    faction select on lobby screen

--- Lobby.svelte
+++ Lobby.svelte
@@ -1,6 +1,6 @@
 <script lang="javascript">
     import Game from './Game.svelte'
-import Gate from './Gate.svelte';
+    import Gate from './Gate.svelte'
     let firebaseConfig = {
         apiKey: "AIzaSyAtutNxHpxtCJi3EUB3irfhNiTfoMu1zLY",
         authDomain: "cw-wars.firebaseapp.com",
@@ -15,12 +15,20 @@ import Gate from './Gate.svelte';
     let promise = firebase.database().ref("game").get()
     promise.then( v => size = v.val().length)
     let newgame=()=>{page='newgame'},join=(key)=>{page=key},off=()=>{page='Game'},back=()=>{},page='menu'
+    let client = new URLSearchParams(window.location.search).get('faction')||'hotseat'
 </script>
 
 <style lang="stylus">
       
 </style>
 {#if (page=='menu') }
+    {#if (client == 'hotseat')}
+        <p> you're currently playing a hotseat pass-and-play game. select your faction to enable online play: </p>
+        <a href='./?faction=ys'>yellow sign</a>
+        <a href='./?faction=gc'>great cthulhu</a>
+        <a href='./?faction=bg'>black goat</a>
+        <a href='./?faction=cc'>crawling chaos</a>
+    {/if}
     <span>games:</span>
     <ul>
         <li on:click={newgame}>new game</li>
